dc2e062330a308493696325be3720e3417e63618
Ruijie Yu <ruijie+git@netyu.xyz>
2022-05-21 11:55:11 -0500

    systemd aliases as array

--- dot_config/zsh/config/50-systemd.zsh
+++ dot_config/zsh/config/50-systemd.zsh
@@ -1,1 +1,14 @@
-alias s=systemctl j=journalctl m=machinectl n=networkctl b=bootctl
+readonly _systemd=(
+    'b=bootctl'
+    'h=hostnamectl'
+    'j=journalctl'
+    'm=machinectl'
+    'n=networkctl'
+    'r=resolvectl'
+    's=systemctl'
+    't=timedatectl'
+    'sm=systemd-mount'
+    'sum=systemd-umount'
+)
+
+alias "${_systemd[@]}"
