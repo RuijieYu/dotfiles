db722f0c6fcee1ed553d5a3c0e25f1c8e372d132
Ruijie Yu <ruijie+git@netyu.xyz>
2022-11-18 02:59:30 -0600

    emacs: olivetti shadows some commonly used orgmode keybinds

--- dot_config/emacs/config.d/50-olivetti.el
+++ dot_config/emacs/config.d/50-olivetti.el
@@ -10,4 +10,10 @@
 (use-package olivetti
   :commands olivetti-mode
   :hook
-  (org-mode . olivetti-mode))
+  (org-mode . olivetti-mode)
+  :config
+  (define-keymap :keymap olivetti-mode-map
+    "C-c" nil
+    "C-c | \\" #'olivetti-set-width
+    "C-c | {" #'olivetti-shrink
+    "C-c | }" #'olivetti-expand))
